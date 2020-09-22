5e70172de7126e04d202de0096e982e35f49953b
Etienne Dldc <e.deladonchamps@gmail.com>
2020-09-22 23:11:57 +0200

    Init database

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,10 +1,13 @@
 import Knex from 'knex';
 import { envs } from '../envs';
+import path from 'path';
+
+export const SQLITE_FILE_PATH = path.resolve(process.cwd(), envs.SQLITE_FILE_PATH);
 
 export const knex = Knex({
   client: 'sqlite3',
   connection: {
-    filename: envs.SQLITE_FILE_PATH,
+    filename: SQLITE_FILE_PATH,
   },
   useNullAsDefault: true,
 });
--- src/database/setup.js
+++ src/database/setup.js
@@ -1,5 +1,53 @@
-import { knex } from './connection';
+import { knex, SQLITE_FILE_PATH } from './connection';
+import fse from 'fs-extra';
+import path from 'path';
+import uuid from '@lukeed/uuid';
 
 export async function setupDatabase() {
-  console.log('TODO');
+  const dbExist = fse.existsSync(SQLITE_FILE_PATH);
+
+  await fse.ensureDir(path.dirname(SQLITE_FILE_PATH));
+
+  const hasMovies = await knex.schema.hasTable('movies');
+  if (!hasMovies) {
+    await knex.schema.createTable('movies', (moviesTable) => {
+      moviesTable.uuid('movie_id').primary();
+      moviesTable.string('title').notNullable();
+      moviesTable.integer('year').notNullable();
+      moviesTable.string('poster_url').defaultTo(null);
+    });
+  }
+
+  const hasReviews = await knex.schema.hasTable('reviews');
+  if (!hasReviews) {
+    await knex.schema.createTable('reviews', (reviewsTable) => {
+      reviewsTable.uuid('review_id').primary();
+      reviewsTable.uuid('movie_id').notNullable().references('movie_id').inTable('movies');
+      reviewsTable.string('author').notNullable();
+      reviewsTable.integer('rating').notNullable();
+      reviewsTable.text('comment');
+    });
+  }
+
+  if (dbExist === false) {
+    // populate with init data
+    const movies = [
+      {
+        movie_id: uuid(),
+        title: 'Inception',
+        year: 2010,
+        poster_url:
+          'https://image.tmdb.org/t/p/w600_and_h900_bestv2/aej3LRUga5rhgkmRP6XMFw3ejbl.jpg',
+      },
+      {
+        movie_id: uuid(),
+        title: 'Arrival',
+        year: 2016,
+        poster_url:
+          'https://image.tmdb.org/t/p/w600_and_h900_bestv2/mSgYBWMybY9Ug4ZnCGsttPIjUx7.jpg',
+      },
+    ];
+
+    await knex.table('movies').insert(movies);
+  }
 }
